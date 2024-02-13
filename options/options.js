445e214587f935129c8f8e6b515ac3d143b8136b
Crowdbotics <team@crowdbotics.com>
2024-02-13 21:44:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "anthropic-constitut-46326",
+  url: "https://anthropic-constitut-46326.botics.co",
+  api: "https://anthropic-constitut-46326.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
